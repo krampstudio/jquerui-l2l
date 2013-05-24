dc1e29b811bb29d3bc3dc87107eaf4f8d6c165d7
Bertrand CHEVRIER <chevrier.bertrand@gmail.com>
2013-05-24 08:06:54 -0400

    add plugin structure

---
+++ src/l2l.js
@@ -0,0 +1,15 @@
+(function($, undefined){
+    
+    $.widget( "ks.l2l", {
+        
+        options: {
+            
+        },
+        
+        _create: function() {
+            //this.options;
+            //this.element
+        }    
+    });
+}(jQuery));
+
