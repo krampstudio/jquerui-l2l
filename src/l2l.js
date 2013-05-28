d9d6429b1911f4ce6e54cb81d3b6555854353528
Bertrand CHEVRIER <chevrier.bertrand@gmail.com>
2013-05-28 08:30:43 -0400

    add controls actions

--- src/l2l.js
+++ src/l2l.js
@@ -6,12 +6,14 @@
             icons : {
                 l2r : "ui-icon-triangle-1-e",
                 r2l : "ui-icon-triangle-1-w",
-                rm : "ui-icon-trash",
+                clear : "ui-icon-trash",
                 grip : "ui-icon-grip-dotted-vertical"
             },
             width: 'auto',
             height: 'auto',
-            sortable : true
+            sortable : true,
+            draggable : true,
+            clear : 'right' //right, left, all or false
         },
         
         _create: function() {
@@ -19,32 +21,32 @@
             
             if(!this.element.hasClass('l2l')){
             
-                var lists = this.element.children("ul");
-                if(lists.length != 2){
+                this.lists = this.element.children("ul");
+                if(this.lists.length != 2){
                     $.error("The l2l element must contains 2 lists as direct child");
                     return false;
                 }
-                var llist = $(lists.get(0));
-                var rlist = $(lists.get(1));
+                this.llist = $(this.lists.get(0));
+                this.rlist = $(this.lists.get(1));
                 
                 this.element.addClass('l2l');
                 
-                lists.addClass('l2l-list ui-widget-content ui-corner-all');
+                this.lists.addClass('l2l-list ui-widget-content ui-corner-all');
                 
                 if(this.options.sortable === true){
-                    lists.sortable({
+                   this.lists.sortable({
                         handle : '.ui-icon',
                         placeholder: 'ui-state-highlight',
                         opacity: 0.7
                     });
                 }
                 
-                lists
+                this.lists
                     .find('li')
                     .addClass('ui-state-default ui-corner-all')
                     .prepend(this._iconHtml('grip'));
                 
-                llist.selectable({ 
+                this.lists.selectable({ 
                         filter: "li", 
                         cancel: ".ui-icon",
                         selected : function(event, ui){
@@ -55,7 +57,7 @@
                         }
                     });
                 
-                llist.after(this._controlsHtml());
+                this.llist.after(this._controlsHtml());
                 
                 this._on(this._events);
             }
@@ -66,29 +68,48 @@
                 event.preventDefault();
                 var control = '_' + $(event.currentTarget).attr('id');
                 if(this[control] !== undefined && typeof this[control] === 'function'){
-                    this[control]();
+                    this[control](event);
                 }
             }
         },
         
-        _l2r : function(){
-            console.log('l2r')
+        _l2r : function(event){
+            this.llist.find('.ui-selected').appendTo(this.rlist);
+            this._change(event);
         },
         
-        _r2l : function(){
-             console.log('z2l')
+        _r2l : function(event){
+            this.rlist.find('.ui-selected').appendTo(this.llist);
+             this._change(event);
         },
         
-        _rm : function(){
-            
+        _clear : function(event){
+            if(this.options.clear !== false){
+                var ctx = (this.options.clear === 'all') ? this.lists : (this.options.clear === 'left') ? this.llist : this.rlist;
+                $('.ui-selected', ctx).remove();
+                 this._change(event);
+            }
+        },
+        
+        _change: function(event){
+             this._trigger("change", event, {
+                elements : [
+                    this.llist.find('li'),
+                    this.rlist.find('li')
+                ]
+            });
         },
         
         _controlsHtml : function(){
-            return  "<ul class='l2l-ctrl'>" +
+            var controls =  "<ul class='l2l-ctrl'>" +
                         "<li id='l2r' class='ui-state-default ui-corner-all'>" +  this._iconHtml('l2r') + "</li>" +
-                        "<li id='r2l' class='ui-state-default ui-corner-all'>" +  this._iconHtml('r2l') + "</li>" +
-                        "<li id='rm' class='ui-state-default ui-corner-all'>" +  this._iconHtml('rm') + "</li>" +
-                    "</ul>";
+                        "<li id='r2l' class='ui-state-default ui-corner-all'>" +  this._iconHtml('r2l') + "</li>";
+            if(this.options.clear !== false){
+                controls += "<li id='clear' class='ui-state-default ui-corner-all'>" +  this._iconHtml('clear') + "</li>";
+            }
+            controls += "</ul>";
+            
+            return controls;
         },
         
         _iconHtml : function(name){
