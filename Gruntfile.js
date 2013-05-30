c855059245ce1e17ff810fdd1c892b20133e2b0d
Bertrand CHEVRIER <chevrier.bertrand@gmail.com>
2013-05-30 07:31:45 -0400

    update build sequence

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,41 +2,54 @@ module.exports = function(grunt){
     'use strict';
     
 	grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        jsdoc: {
+        
+        pkg : grunt.file.readJSON('package.json'),
+        
+        path : {
+            build : 'build/',
+            src : 'src/*.js',
+            css : 'src/*.css' 
+        },
+        
+        clean : ['<%=path.build%>'],
+        
+        jsdoc : {
             dist: {
-                src : 'src/*.js',
-                options: {
-                    destination: 'doc'
+                src : '<%=path.src%>',
+                options : {
+                    destination : '<%=path.build%>doc'
                 }
             }
         },
-        uglify: {
-            options: {
-                banner: '/* <%= pkg.name %> \n'
+        
+        uglify : {
+            options : {
+                banner : '/* <%= pkg.name %> \n'
                         + ' <%= pkg.author.name %> <%= pkg.author.email %> copyright <%= grunt.template.today("yyyy") %> \n'
                         + ' @license <%= pkg.licenses[0].type %> <%= pkg.licenses[0].url %> \n*/\n'
             },
-            build: {
-                src: 'src/js/l2l.js',
-                dest: 'src/js/l2l.min.js'
+            build : {
+                src : '<%=path.src%>',
+                dest : 'src/js/l2l.min.js'
             }
         },
-        jshint: {
-            src : 'src/js/l2l.js',
-            options: {
-                camelcase: true,
-                smarttabs: true,
-                eqnull: true,
-                browser: true,
-                globals: {
-                    jQuery: true
+        
+        jshint : {
+            src : '<%=path.src%>',
+            options : {
+                camelcase : true,
+                smarttabs : true,
+                eqnull : true,
+                browser : true,
+                globals : {
+                    jQuery : true
                 }
             }
         }
 	});
-    
     grunt.loadNpmTasks('grunt-jsdoc');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    
+    grunt.registerTask('default', ['clean', 'jshint', 'jsdoc', 'uglify'])
 };
